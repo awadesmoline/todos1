de83e9e3e8618b8d9e4568171a0ef8b6b82552be
Awa Desmoline <awadesmoline@gmail.com>
2018-06-05 18:30:57 +0100

    list todo items inside todos

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,4 +1,5 @@
-const Todo = require('../models').Todo;
+var Todo = require('../models').Todo;
+var TodoItem = require('../models').TodoItem;
 
 module.exports = {
   create(req, res) {
@@ -7,9 +8,14 @@ module.exports = {
     }).then(todo => res.status(201).send(todo))
     .catch(error => res.status(400).send(error))
   },
-  
+
   list(req, res) {
-    return Todo.all().then(todos => res.status(200).send(todos))
+    return Todo.findAll({
+      include: [{
+        model: TodoItem,
+        as: 'todoItems'
+      }]
+    }).then(todos => res.status(200).send(todos))
     .catch(error => res.status(400).send(error))
   }
 }
