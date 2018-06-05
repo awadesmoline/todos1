1e8b4534a391a3d1566dbb5eca1468ef5a716896
Awa Desmoline <awadesmoline@gmail.com>
2018-06-05 18:06:55 +0100

    List todos

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -6,5 +6,10 @@ module.exports = {
       title: req.body.title
     }).then(todo => res.status(201).send(todo))
     .catch(error => res.status(400).send(error))
+  },
+  
+  list(req, res) {
+    return Todo.all().then(todos => res.status(200).send(todos))
+    .catch(error => res.status(400).send(error))
   }
 }
